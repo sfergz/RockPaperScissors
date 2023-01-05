92c86e4b3270384f6943c5a8b0907daf2d4f5566
sfergz <115808089+sfergz@users.noreply.github.com>
2023-01-05 15:53:19 -0700

    Add game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+// begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+function getComputerChoice () {
+    ('Rock', 'Paper', 'Scissors')
+}
+console.log(getComputerChoice)
+// Write a function that plays a single round of Rock Paper Scissors
+function playRound (playerSelection, computerSelection) {
+    //write my code here
+}
